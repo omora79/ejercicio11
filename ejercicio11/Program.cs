1f3f6a2fcef4186512a6f0476caac8e8dde15a48
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-22 12:18:11 -0500

    definicion de v

---
+++ ejercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ejercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Construir un algoritmo que pida N números por pantalla, cuando el usuario ingrese un
+            //número negativo, el programa debe finalizar y mostrar en consola la cantidad total de
+            //números positivos que ingresó hasta antes de ingresarse el número negativo.
+
+            //1 definir las variables.
+        }
+    }
+}
