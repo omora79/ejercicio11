d175d49f26ed01e9808edd04c70a46974c6a7f4d
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-22 13:10:04 -0500

    termin

---
+++ alterno/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace alterno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Construir un algoritmo que pida N números por pantalla, cuando el usuario ingrese un
+            //número negativo, el programa debe finalizar y mostrar en consola la cantidad total de
+            //números positivos que ingresó hasta antes de ingresarse el número negativo.
+
+            // declaracion de variables
+            int num, conteo = 0;
+
+            Console.WriteLine("Ingrese un numero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while(num >= 0)
+            {
+                conteo++;
+                Console.WriteLine("Ingrese un numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(string.Format("la cantidad de numeros positivos es {0}", conteo));
+
+        }
+
+    }
+}
